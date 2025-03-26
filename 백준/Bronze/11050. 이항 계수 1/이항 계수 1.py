d0da08b4a1ac908cf275eb2c1d9f658a237b2f81
kje0316 <136460735+kje0316@users.noreply.github.com>
2025-03-26 12:02:53 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,4 +1,11 @@
 N, K = map(int, input().split())
 
-import math
-print(int(math.factorial(N)/math.factorial(K)/math.factorial(N-K)))
+# import math
+# print(int(math.factorial(N)/math.factorial(K)/math.factorial(N-K)))
+n = 1
+for i in range(N, N-K, -1):
+    n *= i
+k = 1
+for j in range(1,K+1):
+    k *= j
+print(int(n/k))
